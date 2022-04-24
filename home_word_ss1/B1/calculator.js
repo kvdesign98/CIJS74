48a00f771184b8f90ed58dbd4f99db7106a747e3
vodaihuu42@gmail.com <>
2022-04-24 17:19:03 +0700

    add

--- home_word_ss1/B1/calculator.js
+++ home_word_ss1/B1/calculator.js
@@ -1,16 +1,21 @@
 const PI = 3.14
 
-export const sum = (a, b) => a + b 
-    
+export const sum = (a, b) => a + b
+
 export const calculateRectArea = (a, b) => a * b
 
 export const calculateCircleArea = (r) => PI * r * r
-   
-let info = {
-    'name' : 'tên máy tính',
-    'country' : 'quốc gia sản xuất',
-    'producer' : 'hãng sản xuất',
-    'year' : 'năm sản xuất',
+
+
+
+const info = {
+    "name": "tên máy tính",
+    "country": "quốc gia sản xuất",
+    "producer": "hãng sản xuất",
+    "year": "năm sản xuất",
 }
-export default info 
+
+export default info
+     
+
 

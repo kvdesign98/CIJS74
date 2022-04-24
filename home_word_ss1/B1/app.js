c0f677ca0d9c321f940fb41b52010cd48202528f
vodaihuu42@gmail.com <>
2022-04-24 14:42:12 +0700

    add

---
+++ home_word_ss1/B1/app.js
@@ -0,0 +1,8 @@
+import {sum, calculateRectArea, calculateCircleArea} from "./calculator.js"
+
+sum(12,3);
+
+calculateRectArea(10,5);
+
+calculateCircleArea(30);
+
---
+++ home_word_ss1/B1/calculator.js
@@ -0,0 +1,8 @@
+export const PI = 3.14
+
+export const sum = (a, b) => a + b
+    
+export const calculateRectArea = (a, b) => a * b
+
+export const calculateCircleArea = (r) => PI * r * r
+   
